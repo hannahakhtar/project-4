024656c58b8eebeb3eeeb5af8bb05c205a0be080
jakeaston <jacobaston92@gmail.com>
2021-03-11 18:18:43 +0000

    User and Product tests added

--- server/tests/test_products.py
+++ server/tests/test_products.py
@@ -1,28 +1,36 @@
-# from app import app, db
-# import json
-# from tests.lib import login
+from app import app, db
+import json
+from tests.lib import login
 
-# def test_get_products():
+def test_get_products():
 
-#     client = app.test_client()
-#     response = client.get("/api/products")
+    client = app.test_client()
+    response = client.get("/api/products")
 
-#     assert len(response.json) == 14
-#     assert response.status_code == 200
+    assert len(response.json) == 14
+    assert response.status_code == 200
 
-# def test_bake_cake():
+def test_single_products():
 
-#     client = app.test_client()
+    client = app.test_client()
+    response = client.get("/api/products/1")
 
-#     token = login(client)
+    assert response.json['id'] == 1
+    assert response.status_code == 200    
 
-#     product_data = {"product_name": Nike running shoe", "brand": "Nike", "category": "Shoes", "condition": "Used", "description": "Black running shoe", "gender": "Unisex" "in_stock": True, "price" 15.99 "product_image": "Shoe image"}
-#     request_headers = {"Authorization": f"Bearer {token}"}
-#     product_response = client.post(
-#         "/api/products",
-#         data=json.dumps(product_data),
-#         content_type="application/json",
-#         headers=request_headers,
-#     )
+def test_post_product():
 
-#     assert product_response.json['brand'] == 'Nike'
+    client = app.test_client()
+
+    token = login(client)
+
+    product_data = {"product_name": "Nike running shoe", "brand": "Nike", "category": "Shoes", "condition": "Used", "description": "Black running shoe", "gender": "Unisex", "in_stock": True, "price": 15.99, "product_image": "Shoe image"}
+    request_headers = {"Authorization": f"Bearer {token}"}
+    product_response = client.post(
+        "/api/products",
+        data=json.dumps(product_data),
+        content_type="application/json",
+        headers=request_headers,
+    )
+
+    assert product_response.json['product_name'] == 'Nike running shoe'
--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -1,11 +1,32 @@
-# from app import app, db
-# import json
-# from tests.lib import login
+from app import app, db
+import json
+from tests.lib import login
 
-# def test_get_user():
+def test_get_user():
 
-#     client = app.test_client()
-#     response = client.get("/api/users")
+    client = app.test_client()
+    response = client.get("/api/users")
 
-#     assert len(response.json) == 3
-#     assert response.status_code == 200
+    assert len(response.json) == 3
+    assert response.status_code == 200
+
+def test_single_user():
+
+    client = app.test_client()
+    response = client.get("/api/users/1")
+
+    assert response.json['username'] == 'indiak'
+    assert response.status_code == 200 
+
+def test_signup():
+
+    client = app.test_client()
+
+    product_data = {"username": "sam", "email": "sam@sam.com", "password": "sam123", "first_name": "Sam", "last_name": "Jones", "image": "TODO", "location": "London"}
+    product_response = client.post(
+        "/api/signup",
+        data=json.dumps(product_data),
+        content_type="application/json"
+    )
+
+    assert product_response.json['username'] == 'sam'        
