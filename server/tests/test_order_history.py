d18f05f4031027b36d0f7eb086a16afbd5fbabb6
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-03-11 19:34:14 +0000

    first order history test fixed

--- server/tests/test_order_history.py
+++ server/tests/test_order_history.py
@@ -8,10 +8,11 @@ def test_get_all_order_history():
     token = login(client)
     request_headers = {"Authorization": f"Bearer {token}"}
     response = client.get(
-        "/api/users/3/order-history",
+        "/api/users/2/order-history",
         headers=request_headers,
         )
 
     assert len(response.json) == 2
     assert response.status_code == 200
-    assert response.json[0]["product"].get("brand") == "Zara"
+    assert response.json[0]["product"].get("brand") == "Puma"
+
