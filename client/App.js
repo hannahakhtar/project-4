9c1dd220866fee2c83398f53ea7f2c5299769202
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-03-12 10:53:34 +0000

    frontend - app.js init

--- client/App.js
+++ client/App.js
@@ -3,16 +3,35 @@ import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
 import './styles/style.scss'
 import axios from 'axios'
 
-// ! Some starter code for your frontend, change this
-// ! however you like.
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/test/backend" component={TestBackend} />
-    </Switch>
-  </BrowserRouter>
-)
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Register from './containers/Register'
+import EditProducts from './containers/EditProducts'
+import AddProducts from './containers/AddProducts'
+import SearchHome from './containers/SearchHome'
+import SearchResults from './containers/SearchResults'
+import Profile from './containers/Profile'
+import Checkout from './containers/Checkout'
+import SingleProduct from './containers/SingleProduct'
+
+const App = () => {
+  return <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/edit-products" component={EditProducts} />
+        <Route exact path="/add-product" component={AddProducts} />
+        <Route exact path="/search-home" component={SearchHome} />
+        <Route exact path="/search-results" component={SearchResults} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/products/:id" component={SingleProduct} />
+      </Switch>
+    </BrowserRouter>
+  </>
+}
 
 const Home = () => <Link to={'/test/backend'}>
   Go to /hello/world page.
