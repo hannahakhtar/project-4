f20ad0b78b1b486f05560a9e813ce464348a1525
India Kalff <indiakalff@gmail.com>
2021-03-14 14:25:23 +0000

    user profile

--- client/App.js
+++ client/App.js
@@ -8,7 +8,6 @@ import 'bulma'
 import Home from './containers/Home'
 import Login from './containers/Login'
 import Register from './containers/Register'
-import EditProducts from './containers/EditProducts'
 import ProductForm from './containers/ProductForm'
 import SearchHome from './containers/SearchHome'
 import SearchResults from './containers/SearchResults'
@@ -23,12 +22,11 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/edit-products" component={EditProducts} />
         <Route exact path="/productform" component={ProductForm} />
         <Route exact path="/productform/:productId" component={ProductForm} />
         <Route exact path="/search-home" component={SearchHome} />
         <Route exact path="/search-results" component={SearchResults} />
-        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/profile/:userId" component={Profile} />
         <Route exact path="/checkout" component={Checkout} />
         <Route exact path="/products/:id" component={SingleProduct} />
       </Switch>
