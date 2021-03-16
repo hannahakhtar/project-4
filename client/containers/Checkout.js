708ab69713721abfe37e9c55b0c76c17aa89a432
jakeaston <jacobaston92@gmail.com>
2021-03-16 16:44:46 +0000

    added navbar to checkout

--- client/containers/Checkout.js
+++ client/containers/Checkout.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { getLoggedInUserId } from '../lib/auth.js'
+import Navbar from '../components/Navbar'
 // import nodemailer from 'nodemailer'
 
 function Checkout({ location }) {
@@ -56,7 +57,7 @@ function Checkout({ location }) {
 
   // async function email() {
   //   const testAccount = await nodemailer.createTestAccount()
-  
+
   //   const transporter = nodemailer.createTransport({
   //     host: 'smtp.ethereal.email',
   //     port: 587,
@@ -66,7 +67,7 @@ function Checkout({ location }) {
   //       pass: testAccount.pass
   //     }
   //   })
-  
+
   //   const info = await transporter.sendMail({
   //     from: '"Garms 🧦" <checkout@garms.com>', 
   //     to: 'jacobaston92@gmail.com', 
@@ -74,9 +75,9 @@ function Checkout({ location }) {
   //     text: 'Hello world?', 
   //     html: '<b>Your purchase is complete</b>'
   //   })
-  
+
   //   console.log('Message sent: %s', info.messageId)
-  
+
   //   console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
   // }
 
@@ -84,165 +85,167 @@ function Checkout({ location }) {
     borderRadius: '5%'
   }
 
-  return (<div className="m-4">
-    <div>
-      <h1 className="title has-text-centered m-5">Shipping Details</h1>
-    </div>
-    <div className='container mb-4'>
-      <div className="card">
-        <div className="card-content">
-          <div className="media-left p-0 m-0">
-            <figure>
-              <img className="image" src={'https://cdn.shopify.com/s/files/1/1074/5128/products/vb1713977_Winter-2020---Knitwear---JJ-117_865b831c-b398-4646-bb6e-a0ee0e24d911_1200x.jpg?v=1602769681'} style={imageStyle} />
-            </figure>
-          </div>
-          <div className="media-content mt-2">
-            <h3>{product.product.product_name}</h3>
-            <h3>£{product.product.price}</h3>
+  return <div>
+    <Navbar />
+    <div className="m-4">
+      <div>
+        <h1 className="title has-text-centered m-5">Shipping Details</h1>
+      </div>
+      <div className='container mb-4'>
+        <div className="card">
+          <div className="card-content">
+            <div className="media-left p-0 m-0">
+              <figure>
+                <img className="image" src={product.product.product_image} style={imageStyle} />
+              </figure>
+            </div>
+            <div className="media-content mt-2">
+              <h3>{product.product.product_name}</h3>
+              <h3>£{product.product.price}</h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className='container'>
+      <div className='container'>
 
-      {errorbox && <div className='box has-background-danger has-text-white'>{errorbox}</div>}
+        {errorbox && <div className='box has-background-danger has-text-white'>{errorbox}</div>}
 
-      <form onSubmit={handleSubmit(onSubmit)} className="mb-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="mb-4">
 
-        <h3 className="mt-3 mb-3">Enter your address details</h3>
+          <h3 className="mt-3 mb-3">Enter your address details</h3>
 
-        <div className='field'>
-          <input
-            className={`input ${errors.address1 && 'is-danger'}`}
-            name='address1'
-            placeholder='Address Line 1'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.address1 && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          <div className='field'>
+            <input
+              className={`input ${errors.address1 && 'is-danger'}`}
+              name='address1'
+              placeholder='Address Line 1'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.address1 && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.address2 && 'is-danger'}`}
+              name='address2'
+              placeholder='Address Line 2'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.address2 && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.city && 'is-danger'}`}
+              name='city'
+              placeholder='City'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.city && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.post_code && 'is-danger'}`}
+              name='post_code'
+              placeholder='Post code'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.post_code && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.phone && 'is-danger'}`}
+              name='phone'
+              placeholder='Phone (Optional)'
+              type='text'
+              defaultValue=''
+              ref={register({ required: false })}
+            />
+            {errors.phone && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <h3 className="mt-3 mb-3">Enter your card details</h3>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.card && 'is-danger'}`}
+              name='card'
+              placeholder='16 Digit Card Number'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.card && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.expire && 'is-danger'}`}
+              name='expire'
+              placeholder='Example 10/20'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.expire && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.name && 'is-danger'}`}
+              name='name'
+              placeholder='Name on card'
+              type='text'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.name && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <div className='field'>
+            <input
+              className={`input ${errors.security && 'is-danger'}`}
+              name='security'
+              placeholder='3 Digit security code'
+              type='password'
+              defaultValue=''
+              ref={register({ required: true })}
+            />
+            {errors.security && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
+          </div>
+
+          <h3>Shipping: £{shipping}</h3>
+          <h3>Total plus shipping:</h3>
+          <h3>£{(product.product.price + shipping).toFixed(2)}</h3>
+
+          {/* {product.product.id && <Link to={'/search-home'}> */}
+          <button
+            className='button is-info mr-3'
+            onClick={handleSubmitProduct}
+          >
+            Continue
+          </button>
+          {/* </Link>} */}
+
+        </form >
+
+        <Link to={`products/${product.product.id}`}><button className='button is-info'>Continue Shopping</button></Link>
+        <div className='mt-4'>
+          <small>Please note, this is just a project and real card details should not be entered</small>
         </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.address2 && 'is-danger'}`}
-            name='address2'
-            placeholder='Address Line 2'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.address2 && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.city && 'is-danger'}`}
-            name='city'
-            placeholder='City'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.city && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.post_code && 'is-danger'}`}
-            name='post_code'
-            placeholder='Post code'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.post_code && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.phone && 'is-danger'}`}
-            name='phone'
-            placeholder='Phone (Optional)'
-            type='text'
-            defaultValue=''
-            ref={register({ required: false })}
-          />
-          {errors.phone && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <h3 className="mt-3 mb-3">Enter your card details</h3>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.card && 'is-danger'}`}
-            name='card'
-            placeholder='16 Digit Card Number'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.card && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.expire && 'is-danger'}`}
-            name='expire'
-            placeholder='Example 10/20'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.expire && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.name && 'is-danger'}`}
-            name='name'
-            placeholder='Name on card'
-            type='text'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.name && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <div className='field'>
-          <input
-            className={`input ${errors.security && 'is-danger'}`}
-            name='security'
-            placeholder='3 Digit security code'
-            type='password'
-            defaultValue=''
-            ref={register({ required: true })}
-          />
-          {errors.security && <div className='mt-2 mb-2 is-size-7'>This field is required</div>}
-        </div>
-
-        <h3>Shipping: £{shipping}</h3>
-        <h3>Total plus shipping:</h3>
-        <h3>£{(product.product.price + shipping).toFixed(2)}</h3>
-
-        {/* {product.product.id && <Link to={'/search-home'}> */}
-        <button
-          className='button is-info mr-3'
-          onClick={handleSubmitProduct}
-        >
-          Continue
-        </button>
-        {/* </Link>} */}
-
-      </form >
-
-      <Link to={`products/${product.product.id}`}><button className='button is-info'>Continue Shopping</button></Link>
-      <div className='mt-4'>
-        <small>Please note, this is just a project and real card details should not be entered</small>
       </div>
     </div>
   </div>
-  )
 }
 
 export default Checkout
