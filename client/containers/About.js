e5b4e202d75cf21842ee90f762f529608860d749
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-03-16 21:52:51 +0000

    changes to categories carousel and added aan about page

--- client/containers/About.js
+++ client/containers/About.js
@@ -2,35 +2,78 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 
 function About() {
+
+  const techInfo = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png',
+      alt: 'HTML5'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png',
+      alt: 'CSS3'
+    },
+    {
+      url: 'https://bulma.io/images/bulma-logo.png',
+      alt: 'Bulma'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+      alt: 'JavaScript'
+    },
+    {
+      url: 'https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png',
+      alt: 'React'
+    },
+    {
+      url: 'https://www.postgresql.org/media/img/about/press/elephant.png',
+      alt: 'PostgreSQL'
+    },
+    {
+      url: 'https://docs.pytest.org/en/stable/_static/pytest1.png',
+      alt: 'PyTest'
+    },
+    {
+      url: 'https://marshmallow.readthedocs.io/en/stable/_static/marshmallow-logo.png',
+      alt: 'Marshmallow'
+    },
+    {
+      url: 'https://flask.palletsprojects.com/en/1.1.x/_images/flask-logo.png',
+      alt: 'Flask'
+    },
+    {
+      url: 'https://www.sqlalchemy.org/img/sqla_logo.png',
+      alt: 'SQLAlchemy'
+    }
+    
+  ]
+
+  const tech = techInfo.map(t => {
+    return <div className={'tech-card'} key={t.alt}>
+      <img src={t.url} />
+    </div>
+  })
+
   return <>
     <Navbar />
     <div className="container">
-      <h2>About Garms</h2>
-      <p>This is all about Garms</p>
-    </div>
-    <div className="container">
-      <h2>Created By</h2>
-      <p><a href="https://github.com/hannahakhtar">Hannah Akhtar</a>, <a href="https://github.com/ikalff">India</a> & <a href="https://github.com/Jacobaston">Jake</a> for project 4 of <a href="https://generalassemb.ly/">General Assembly’s</a> Software Engeering Immersive.</p>
-    </div>
-    <div className="container">
-      <h2>Technologies</h2>
+      <div className="hero is-fullheight-with-navbar">
+        <div className="box about-page about-page-box-1">
+          <h2 className="about-h2">About Garms</h2>
+          <p className="about-p">Garms is an application for users to buy and sell clothes, accessories and shoes.</p>
+        </div>
+        <div className="box about-page">
+          <h2 className="about-h2">Founders</h2>
+          <p className="about-p">Garms was founded by <a href="https://github.com/hannahakhtar">Hannah Akhtar</a>, <a href="https://github.com/ikalff">India Kalff</a> & <a href="https://github.com/Jacobaston">Jake Aston</a> for project 4 of <a href="https://generalassemb.ly/">General Assembly’s</a> Software Engeering Immersive.</p>
+        </div>
+        <div className="box about-page about-page-box-3">
+          <h2 className="about-h2">Technologies</h2>
+          <div className={'tech-list-container'}>
+            {tech}
+          </div>
+        </div>
+      </div>
     </div>
   </>
 }
-
-
-// return <>
-// <Navbar />
-// <div className="hero is-fullheight-with-navbar">
-//   <div className="order-confirmation-div">
-//     <div className="order-confirmation-container container pt-5 pb-5 px-4">
-//       <p className="order-confirmation">Thank you for your order.</p>
-//       <p className="order-confirmation">Check your emails for confirmation and shipping updates.</p>
-//       <Link className="button is-primary order-confirmation-button" to={{ pathname: '/search-home' }}>Back to search</Link>
-//     </div>
-//   </div>
-// </div>
-// </>
-// }
 
 export default About
