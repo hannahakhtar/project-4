45f740ebe206cb5efc97ac08023ca333c59f7633
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-03-17 14:13:19 +0000

    changes to fb share

--- client/components/facebookShare.js
+++ client/components/facebookShare.js
@@ -1,11 +1,9 @@
 import React from 'react'
-// import FaFacebook from 'react-icons/lib/fa/facebook'
 import { FacebookShareButton } from 'react-share'
 
 export default function ShareButtonFacebook({ productId }) {
   const shareButtonProps = {
     url: `https://garms-shopping.herokuapp.com/products/${productId}`,
-    // change url and deployment link`,
     network: 'Facebook',
     text: 'Check out this cool listing on Garms!',
     longtext:
