5cf85646bbca14c7f2633b69a6f172c9b99b5ed4
India Kalff <indiakalff@gmail.com>
2021-03-17 11:14:29 +0000

    single products

--- client/containers/SingleProduct.js
+++ client/containers/SingleProduct.js
@@ -126,16 +126,13 @@ function SingleProduct({ match, history }) {
           <section className="content m-3">
             <div>
               <h3 className="title"><strong>{product.product_name}</strong></h3>
-
-              <p><strong>Description:</strong> {product.description}</p>
             </div>
             <div>
-              <p><strong>Size:</strong> {product.size}</p>
-              <p><strong>Condition:</strong> {product.condition}</p>
-              <p><strong>Brand:</strong> {product.brand}</p>
-            </div>
-            <div>
-              <strong><p>Price: £{product.price}</p></strong>
+              <p className='block'><strong>Description:</strong> {product.description}</p>
+              <p className='block'><strong>Size:</strong> {product.size}</p>
+              <p className='block'><strong>Condition:</strong> {product.condition}</p>
+              <p className='block'><strong>Brand:</strong> {product.brand}</p>
+              <p className='block'><strong>Price:</strong> £{product.price}</p>
             </div>
             <div className='buttons mt-5'>
               {handleInStock()}
